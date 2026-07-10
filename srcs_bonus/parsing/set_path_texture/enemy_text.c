5c8b67e2a48fe254973714b247cf91fdbe13d9f0
Sammy Maouche <samaouch@z2r9p5.42lyon.fr>
2026-07-10 09:06:23 +0200

    change sorcerer asset

--- srcs_bonus/parsing/set_path_texture/enemy_text.c
+++ srcs_bonus/parsing/set_path_texture/enemy_text.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   enemy_text.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ale-guel <ale-guel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: samaouch <samaouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 07:57:36 by ale-guel          #+#    #+#             */
-/*   Updated: 2025/12/10 07:57:37 by ale-guel         ###   ########.fr       */
+/*   Updated: 2026/07/09 14:29:26 by samaouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,23 @@ static void	set_path_spell(t_data *data)
 	data->img[WEB_SPIDER_IMG].path = "./texture/web.xpm";
 }
 
-static void	set_path_front(t_data *data)
-{
-	data->img[BIRD_FRONT].path = "./texture/bird_front.xpm";
-	data->img[ELEM_FRONT].path = "./texture/elem_front.xpm";
-	data->img[WOLF_FRONT].path = "./texture/wolf_front.xpm";
-	data->img[SPIDER_FRONT].path = "./texture/spider_front.xpm";
-	data->img[KUNFANDI_FRONT].path = "./texture/kunfandi_front.xpm";
-	data->img[NANCY_FRONT].path = "./texture/nancy_front.xpm";
-	data->img[DEMENTOR_FRONT].path = "./texture/dementor_front.xpm";
-	data->img[PILO_FRONT].path = "./texture/pilo_front.xpm";
-	data->img[ZIPPY_FRONT].path = "./texture/zippy_front.xpm";
-	data->img[STEF_FRONT].path = "./texture/stef_front.xpm";
-	data->img[DIRLO_FRONT].path = "./texture/dirlo_front.xpm";
-	data->img[CAMEO_FRONT].path = "./texture/cameo_front.xpm";
-	data->img[ANAIS_FRONT].path = "./texture/anais_front.xpm";
-	data->img[MARINA_FRONT].path = "./texture/marina_front.xpm";
-}
+// static void	set_path_front(t_data *data)
+// {
+// 	data->img[BIRD_FRONT].path = "./texture/bird_front.xpm";
+// 	data->img[ELEM_FRONT].path = "./texture/elem_front.xpm";
+// 	data->img[WOLF_FRONT].path = "./texture/wolf_front.xpm";
+// 	data->img[SPIDER_FRONT].path = "./texture/spider_front.xpm";
+// 	data->img[KUNFANDI_FRONT].path = "./texture/kunfandi_front.xpm";
+// 	data->img[NANCY_FRONT].path = "./texture/nancy_front.xpm";
+// 	data->img[DEMENTOR_FRONT].path = "./texture/dementor_front.xpm";
+// 	data->img[PILO_FRONT].path = "./texture/pilo_front.xpm";
+// 	data->img[ZIPPY_FRONT].path = "./texture/zippy_front.xpm";
+// 	data->img[STEF_FRONT].path = "./texture/stef_front.xpm";
+// 	data->img[DIRLO_FRONT].path = "./texture/dirlo_front.xpm";
+// 	data->img[CAMEO_FRONT].path = "./texture/cameo_front.xpm";
+// 	data->img[ANAIS_FRONT].path = "./texture/anais_front.xpm";
+// 	data->img[MARINA_FRONT].path = "./texture/marina_front.xpm";
+// }
 
 static void	set_path_back(t_data *data)
 {
@@ -63,6 +63,51 @@ static void	set_path_back(t_data *data)
 	data->img[SORCERER_BACK_45].path = "./texture/sorcerer_back_45.xpm";
 }
 
+// static void	set_path_side(t_data *data)
+// {
+// 	data->img[BIRD_SIDE_90].path = "./texture/bird_90.xpm";
+// 	data->img[ELEM_SIDE_90].path = "./texture/elem_90.xpm";
+// 	data->img[WOLF_SIDE_90].path = "./texture/wolf_90.xpm";
+// 	data->img[SPIDER_SIDE_90].path = "./texture/spider_90.xpm";
+// 	data->img[SNAKE_SIDE_90].path = "./texture/snake_90.xpm";
+// 	data->img[SORCERER_SIDE_90].path = "./texture/sorcerer_90.xpm";
+// 	data->img[DEMENTOR_SIDE_90].path = "./texture/dementor_90.xpm";
+// 	data->img[PILO_FRONT_45].path = "./texture/pilo_front_45.xpm";
+// 	data->img[ANAIS_FRONT_45].path = "./texture/anais_front_45.xpm";
+// 	data->img[DIRLO_FRONT_45].path = "./texture/dirlo_front_45.xpm";
+// 	data->img[STEF_FRONT_45].path = "./texture/stef_front_45.xpm";
+// 	data->img[ZIPPY_FRONT_45].path = "./texture/zippy_front_45.xpm";
+// 	data->img[CAMEO_FRONT_45].path = "./texture/cameo_front_45.xpm";
+// 	data->img[NANCY_FRONT_45].path = "./texture/nancy_front_45.xpm";
+// 	data->img[KUNFANDI_FRONT_45].path = "./texture/kunfandi_front_45.xpm";
+// 	data->img[MARINA_FRONT_45].path = "./texture/marina_front_45.xpm";
+// 	data->img[SPIDER_FRONT_45].path = "./texture/spider_front_45.xpm";
+// 	data->img[SNAKE_FRONT].path = "./texture/snake_front.xpm";
+// 	data->img[SNAKE_FRONT_45].path = "./texture/snake_front_45.xpm";
+// 	data->img[DEMENTOR_FRONT_45].path = "./texture/dementor_front_45.xpm";
+// 	data->img[WOLF_FRONT_45].path = "./texture/wolf_front_45.xpm";
+// 	data->img[BIRD_FRONT_45].path = "./texture/bird_front_45.xpm";
+// 	data->img[ELEM_FRONT_45].path = "./texture/elem_front_45.xpm";
+// }
+
+static void	set_path_front(t_data *data)
+{
+	data->img[BIRD_FRONT].path = "./texture/bird_front.xpm";
+	data->img[ELEM_FRONT].path = "./texture/elem_front.xpm";
+	data->img[WOLF_FRONT].path = "./texture/wolf_front.xpm";
+	data->img[SPIDER_FRONT].path = "./texture/spider_front.xpm";
+	data->img[KUNFANDI_FRONT].path = "./texture/anonym_front.xpm";
+	data->img[NANCY_FRONT].path = "./texture/anonym_front.xpm";
+	data->img[DEMENTOR_FRONT].path = "./texture/dementor_front.xpm";
+	data->img[PILO_FRONT].path = "./texture/anonym_front.xpm";
+	data->img[ZIPPY_FRONT].path = "./texture/anonym_front.xpm";
+	data->img[STEF_FRONT].path = "./texture/anonym_front.xpm";
+	data->img[DIRLO_FRONT].path = "./texture/anonym_front.xpm";
+	data->img[CAMEO_FRONT].path = "./texture/anonym_front.xpm";
+	data->img[ANAIS_FRONT].path = "./texture/anonym_front.xpm";
+	data->img[MARINA_FRONT].path = "./texture/anonym_front.xpm";
+}
+
 static void	set_path_side(t_data *data)
 {
 	data->img[BIRD_SIDE_90].path = "./texture/bird_90.xpm";
@@ -72,15 +117,15 @@ static void	set_path_side(t_data *data)
 	data->img[SNAKE_SIDE_90].path = "./texture/snake_90.xpm";
 	data->img[SORCERER_SIDE_90].path = "./texture/sorcerer_90.xpm";
 	data->img[DEMENTOR_SIDE_90].path = "./texture/dementor_90.xpm";
-	data->img[PILO_FRONT_45].path = "./texture/pilo_front_45.xpm";
-	data->img[ANAIS_FRONT_45].path = "./texture/anais_front_45.xpm";
-	data->img[DIRLO_FRONT_45].path = "./texture/dirlo_front_45.xpm";
-	data->img[STEF_FRONT_45].path = "./texture/stef_front_45.xpm";
-	data->img[ZIPPY_FRONT_45].path = "./texture/zippy_front_45.xpm";
-	data->img[CAMEO_FRONT_45].path = "./texture/cameo_front_45.xpm";
-	data->img[NANCY_FRONT_45].path = "./texture/nancy_front_45.xpm";
-	data->img[KUNFANDI_FRONT_45].path = "./texture/kunfandi_front_45.xpm";
-	data->img[MARINA_FRONT_45].path = "./texture/marina_front_45.xpm";
+	data->img[PILO_FRONT_45].path = "./texture/anonym_front_45.xpm";
+	data->img[ANAIS_FRONT_45].path = "./texture/anonym_front_45.xpm";
+	data->img[DIRLO_FRONT_45].path = "./texture/anonym_front_45.xpm";
+	data->img[STEF_FRONT_45].path = "./texture/anonym_front_45.xpm";
+	data->img[ZIPPY_FRONT_45].path = "./texture/anonym_front_45.xpm";
+	data->img[CAMEO_FRONT_45].path = "./texture/anonym_front_45.xpm";
+	data->img[NANCY_FRONT_45].path = "./texture/anonym_front_45.xpm";
+	data->img[KUNFANDI_FRONT_45].path = "./texture/anonym_front_45.xpm";
+	data->img[MARINA_FRONT_45].path = "./texture/anonym_front_45.xpm";
 	data->img[SPIDER_FRONT_45].path = "./texture/spider_front_45.xpm";
 	data->img[SNAKE_FRONT].path = "./texture/snake_front.xpm";
 	data->img[SNAKE_FRONT_45].path = "./texture/snake_front_45.xpm";
@@ -89,7 +134,6 @@ static void	set_path_side(t_data *data)
 	data->img[BIRD_FRONT_45].path = "./texture/bird_front_45.xpm";
 	data->img[ELEM_FRONT_45].path = "./texture/elem_front_45.xpm";
 }
-
 void	set_path_enemy_texture(t_data *data)
 {
 	set_path_spell(data);
